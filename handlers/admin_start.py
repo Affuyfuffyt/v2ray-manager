0d36db622525f5e7aee1089598da0eb3b664ebc1
Affuyfuffyt <98175855+Affuyfuffyt@users.noreply.github.com>
2026-04-25 12:53:10 +0300

    Update admin_start.py

--- handlers/admin_start.py
+++ handlers/admin_start.py
@@ -1,11 +1,14 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def show_main_menu(bot, chat_id):
-    # إنشاء الأزرار الرئيسية
-    markup = InlineKeyboardMarkup(row_width=2)
-    btn_users = InlineKeyboardButton("👥 إدارة المشتركين", callback_data="manage_users")
+    # إنشاء الأزرار الرئيسية (خلينا العرض 1 حتى تترتب الأزرار بشكل عمودي وأنيق)
+    markup = InlineKeyboardMarkup(row_width=1)
+    
     btn_create = InlineKeyboardButton("➕ إنشاء كود جديد", callback_data="create_code")
-    markup.add(btn_users, btn_create)
+    btn_users = InlineKeyboardButton("👥 إدارة المشتركين", callback_data="manage_users")
+    btn_test = InlineKeyboardButton("📈 فحص الاستهلاك المباشر (Live Test)", callback_data="live_speed_test") # 👈 الزر الجديد
+    
+    markup.add(btn_create, btn_users, btn_test)
     
     # إرسال الرسالة
     bot.send_message(
